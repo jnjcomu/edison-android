79aef89c617b3eaac3b9bba16836a5bbf4b696c2
김우재 / WooJae Kim <wj1187@naver.com>
2017-04-06 21:19:23 +0900

    LoadJson.java added

--- app/src/main/java/com/jnjcomu/edison/MainActivity.java
+++ app/src/main/java/com/jnjcomu/edison/MainActivity.java
@@ -2,6 +2,12 @@ package com.jnjcomu.edison;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jnjcomu.edison.module.Encrypt;
+import com.jnjcomu.edison.module.LoadJson;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Edison.json 불러오기
+        String[] s =LoadJson.loadloplat(getApplicationContext());
+        Log.d("id", s[0]);
+        Log.d("pw", s[1]);
+
+        //암호화 코드
+        /*byte b1[] = null, b2[] = null;
+        try{
+            b1 = s[0].getBytes("utf-8");
+            b2 = s[1].getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        Log.d("id_d", Encrypt.encrypt(getApplicationContext(), b1));
+        Log.d("pw_d", Encrypt.encrypt(getApplicationContext(), b2));*/
+
+        //복호화 코드
+        Log.d("id_e", Encrypt.decrypt(getApplicationContext(), s[0]));
+        Log.d("id_d", Encrypt.decrypt(getApplicationContext(), s[1]));
     }
+
 }
---
+++ app/src/main/java/com/jnjcomu/edison/module/LoadJson.java
@@ -0,0 +1,49 @@
+package com.jnjcomu.edison.module;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by kimwoojae on 2017. 4. 5.
+ */
+
+public class LoadJson {
+
+    public static String[] loadloplat(Context mContext) {
+        String[] ls = new String[2];
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset(mContext));
+            JSONObject edison = obj.getJSONObject("Edison");
+            JSONObject jo_inside = edison.getJSONObject("loplat");
+            String id_value = jo_inside.getString("id");
+            String pw_value = jo_inside.getString("pw");
+            ls[0] = id_value;
+            ls[1] = pw_value;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ls;
+    }
+
+    private static String loadJSONFromAsset(Context mContext) {
+        String json;
+        try {
+            InputStream is = mContext.getAssets().open("Edison.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+}
