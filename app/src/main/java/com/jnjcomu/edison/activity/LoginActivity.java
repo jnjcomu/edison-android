8498a2753fc32587f80fb644ff7f2b8e2108fcbb
CodeRi13 <ruto1924@gmail.com>
2017-05-16 22:20:40 +0900

    add login activity

--- app/src/main/java/com/jnjcomu/edison/activity/LoginActivity.java
+++ app/src/main/java/com/jnjcomu/edison/activity/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.jnjcomu.edison;
+package com.jnjcomu.edison.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jnjcomu.edison.R;
 
 import org.androidannotations.annotations.EActivity;
 
