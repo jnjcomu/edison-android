7052ea5ce6a2f87229ae4ea5da280406e6b270b3
CodeRi13 <ruto1924@gmail.com>
2017-07-10 20:04:10 +0900

    Modify Edison.json file name to edison.json

--- app/src/main/java/com/jnjcomu/edison/storage/Configuration.kt
+++ app/src/main/java/com/jnjcomu/edison/storage/Configuration.kt
@@ -54,6 +54,6 @@ class Configuration(context: Context) {
     companion object {
         private val TAG = "Configuration"
         private val ENCODING = "UTF-8"
-        private val CONFIGURATION_JSON_NAME = "Edison.json"
+        private val CONFIGURATION_JSON_NAME = "edison.json"
     }
 }
