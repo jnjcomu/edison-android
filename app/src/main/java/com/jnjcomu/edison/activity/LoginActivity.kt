98de957c21ad35109311a67d77f48d91746a0619
CodeRi13 <ruto1924@gmail.com>
2017-07-12 22:31:19 +0900

    Add LoginActivity kt

--- app/src/main/java/com/jnjcomu/edison/activity/LoginActivity.kt
+++ app/src/main/java/com/jnjcomu/edison/activity/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.jnjcomu.edison.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.jnjcomu.edison.R
 
 /*
  * NOTE
@@ -9,8 +11,15 @@ import android.support.v7.app.AppCompatActivity
  * 져야 한다는 Google Material Design Guideline 에 따라 Deprecated 됨.
  * 그리하여 로그인 버튼 하단에 있는 ProgressBar 로 대체함.
  */
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+    override fun onClick(v: View) {
+
     }
 }
