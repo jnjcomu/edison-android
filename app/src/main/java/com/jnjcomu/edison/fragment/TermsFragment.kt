44531ed425d9db380107e800b07f5c52832014dc
김우재 / WooJae Kim <wj1187@naver.com>
2017-10-26 19:36:03 +0900

    약관

--- app/src/main/java/com/jnjcomu/edison/fragment/TermsFragment.kt
+++ app/src/main/java/com/jnjcomu/edison/fragment/TermsFragment.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.AppCompatCheckBox
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.Toast
 import com.jnjcomu.edison.R
 import com.jnjcomu.edison.activity.CheckInActivity
@@ -21,8 +22,12 @@ class TermsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_terms, container, false)
 
-        view!!.findViewById<View>(R.id.start).setOnClickListener{
-            if(view.findViewById<AppCompatCheckBox>(R.id.agree).isChecked) {
+        view!!.findViewById<WebView>(R.id.terms_1).loadUrl("file:///android_asset/terms.html")
+        view.findViewById<WebView>(R.id.terms_2).loadUrl("file:///android_asset/terms_privacy.html")
+
+        view.findViewById<View>(R.id.start).setOnClickListener{
+            if(view.findViewById<AppCompatCheckBox>(R.id.agree_1).isChecked &&
+                    view.findViewById<AppCompatCheckBox>(R.id.agree_2).isChecked) {
                 startActivity(Intent(activity, CheckInActivity::class.java))
             } else {
                 Toast.makeText(activity,"약관에 모두 동의해야 Edison 서비스를 이용하실 수 있습니다.", Toast.LENGTH_SHORT).show()
